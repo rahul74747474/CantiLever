e288d1bd18e7d8b325c06e058a15105e0f465a7f
rahul74747474 <ghibli141525@gmail.com>
2025-08-16 15:44:44 +0530

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,6 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   root: ".",
-  base: "/",   // 👈 required for Vercel SPA deployment
+  base: "/",   // required for SPA on Vercel
   server: {
     host: "::",
     port: 8080,
